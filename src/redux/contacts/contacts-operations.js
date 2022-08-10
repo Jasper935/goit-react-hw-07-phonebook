7bc0dfed2b52c6e9e5bf9f6057b99ca61ca3339a
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-08-10 15:51:18 +0300

    upd

--- src/redux/contacts/contacts-operations.js
+++ src/redux/contacts/contacts-operations.js
@@ -1,28 +1,8 @@
 import axios from "axios"
 import { createAsyncThunk } from '@reduxjs/toolkit'
-// import { userAPI } from './userAPI'
-// import {
-//     // fetchContactsRequest,
-//     // fetchContactsSuccess,
-//     // fetchContactsError,
-//     // addContactRequest,
-//     // addContactSuccess,
-//     // addContactError,
-//     // deleteContactRequest,
-//     // deleteContactSuccess,
-//     // deleteContactError
-// } from '../contacts/contacts-actions'
-axios.defaults.baseURL = 'https://62ed48d7818ab252b60e1d89.mockapi.io/contacts'
-// export const fetchContacts = () => async (dispatch) => {
-//     dispatch(fetchContactsRequest())
-//     try {
-//         const { data } = await axios.get('/contacts')
 
-//         dispatch(fetchContactsSuccess(data))
-//     } catch (error) {
-//         dispatch(fetchContactsError(error))
-//     }
-// }
+axios.defaults.baseURL = 'https://62ed48d7818ab252b60e1d89.mockapi.io/contacts'
+
 
 export const fetchContacts = createAsyncThunk(
     'contacts/fetchContacts', //под капотом создаст статусы
